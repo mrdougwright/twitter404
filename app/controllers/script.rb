e970970462529b9f5058118983e3fa500a8b7ac8
Doug Wright <mrdougwright@gmail.com>
2013-06-26 13:52:43 -0700

    Add twitter get script

--- app/controllers/script.rb
+++ app/controllers/script.rb
@@ -1,4 +1,34 @@
 require 'rubygems'
 require 'oauth'
 
-consumer_key = OAuth::Consumer.new()
+# The consumer key identifies the application making the request.
+# The access token identifies the user making the request.
+consumer_key = OAuth::Consumer.new(
+  ENV["TWIT_CONSUMER_KEY"],
+  ENV["TWIT_CONSUMER_SECRET"])
+access_token = OAuth::Token.new(
+  ENV["TWIT_TOKEN"],
+  ENV["TWIT_SECRET"])
+
+# All requests will be sent to this server.
+baseurl = "https://api.twitter.com"
+
+# The verify credentials endpoint returns a 200 status if
+# the request is signed correctly.
+address = URI("#{baseurl}/1.1/account/verify_credentials.json")
+
+# Set up Net::HTTP to use SSL, which is required by Twitter.
+http = Net::HTTP.new address.host, address.port
+http.use_ssl = true
+http.verify_mode = OpenSSL::SSL::VERIFY_PEER
+
+# Build the request and authorize it with OAuth.
+request = Net::HTTP::Get.new address.request_uri
+request.oauth! http, consumer_key, access_token
+
+# Issue the request and return the response.
+http.start
+response = http.request request
+puts "The response status was #{response.code}"
+puts JSON.response
+
