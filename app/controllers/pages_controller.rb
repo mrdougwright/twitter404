6b4cc28696bde61fe6099311a69f66a75e8cf3af
Doug Wright <mrdougwright@gmail.com>
2013-06-27 08:59:42 -0700

    move logic into model

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,5 @@
 class PagesController < ApplicationController
   def home
-    @tweets = GetTweets.get_tweets("mrdougwright")
+    @tweets = GetTweets.get_tweets("I love Jane")
   end
 end
--- app/models/get_tweets.rb
+++ app/models/get_tweets.rb
@@ -2,7 +2,7 @@ class GetTweets < ActiveRecord::Base
   attr_accessible :tweet
 
   def self.get_tweets(query)
-    Twitter.search(query, :count => 3, :result_type => "recent").results.map do |status|
+    Twitter.search(query, :lang => "en", :result_type => "recent").results.map do |status|
       "#{status.from_user}: #{status.text}"
     end
   end
